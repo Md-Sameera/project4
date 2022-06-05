fe675f557755b1d21d7573fdd56ca9ddef59c01e
sameera <48850057+Md-Sameera@users.noreply.github.com>
2022-06-05 23:53:42 +0530

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,77 +1,95 @@
-function clock(){
-    var hour=document.getElementById("hr");
-    var minute=document.getElementById("min");
-    var second=document.getElementById("sec");
-    var ampm=document.getElementById("ap");
+function clock()
+{
+    var hours = document.getElementById("hour");
+    var minutes = document.getElementById("min");
+    var seconds = document.getElementById("sec");
+    var am = document.getElementById("ampm");
 
     var time=new Date();
-    var hrs=time.getHours();
-    var mins=time.getMinutes();
-    var secs=time.getSeconds();
-    var apm="AM";
+    var hrs = time.getHours();
+    var mins = time.getMinutes();
+    var secs = time.getSeconds();
+    var am_pm = "AM";
 
-    if (hrs==0) {
-        hrs=12;
+
+    if (hrs == 0) {
+        hrs = 12;
     }
-    if (hrs>12) {
-        hrs=hrs-12;
-        apm="PM";
+
+    if (hrs > 12) {
+        hrs = hrs - 12;
+        am_pm = "PM";
     }
-    if (hrs<10) {
-        hrs="0"+hrs;
+    if (hrs < 10) {
+        hrs = "0" + hrs;
     }
-    if (mins<10) {
-        mins="0"+mins;
+    if (mins < 10) {
+        mins = "0" + mins;
     }
-    if (secs<10) {
-        secs="0"+secs;
+    if (secs < 10) {
+        secs = "0" + secs;
     }
-    hour.innerText = hrs;
-    minute.innerText=mins;
-    second.innerText=secs;
-    ampm.innerText=apm;
+    hours.innerText = hrs;
+    minutes.innerText = mins;
+    seconds.innerText = secs;
+    am.innerText = am_pm;
 }
 setInterval(clock,1000)
 
+function setAlarm()
+{
 
-function settime(){
-    var ab=document.getElementById("wu").value;
-    // var c1=ab;
-    // document.getElementById("out").innerText=c1;
-    var cd=document.getElementById("lun").value;
-    // var c2=cd;
-    // document.getElementById("outp").innerText=c2;
-    var ef=document.getElementById("np").value;
-    // var c3=ef;
-    // document.getElementById("outpu").innerText=c3;
-    var gh=document.getElementById("n8").value;
-    // var c4=gh;
-    // document.getElementById("output").innerText=c4;
-        
-    let hr=new Date().getHours();
-    if(ab==hr){
-        // document.getElementById("image").src="morning.svg";
-        document.getElementById("box7").innerText='GRAB SOME HEALTHY BREAKFAST!!.';
-        document.getElementById("box3").innerText="Good Morning !! Wake Up  !!";
+    var x=document.getElementById('wu').value;
+    var y = document.getElementById("lun").value;
+    var z = document.getElementById("np").value;
+    var j = document.getElementById("n8").value;
+
+    var hour=new Date().getHours();
+
+    if(x==hour){
+        document.getElementById("image").style.backgroundImage="url(./morning.svg)"
+        document.getElementById("textb").innerText="GRAB SOME HEALTHY BREAKFAST!!."
+        document.getElementById("texta").innerText="Good Morning !! Wake Up  !!"
         
     }
-    if(cd==hr){
-        // document.getElementById("image").src="lunch.svg";
-        document.getElementById("box7").innerText='LETS HAVE SOME LUNCH  !!.';
-        document.getElementById("box3").innerText="Good Afternoon Take some sleep";
+    if(hour==y){
+        document.getElementById("image").style.backgroundImage="url(./lunch.svg)"
+        document.getElementById("textb").innerText="LETS HAVE SOME LUNCH  !!."
+        document.getElementById("texta").innerText="Good Afternoon !! Take some sleep"
        
     }
-    if(ef==hr){
-        // document.getElementById("image").src="nap.png";
-        document.getElementById("box7").innerText='STOP YAWNING , ITS JUST AFTERNOON ,GET SOME TEA  !!.';
-        document.getElementById("box3").innerText="Good Evening";
+    if(z==hour){
+        document.getElementById("image").style.backgroundImage="url(./cup.png)"
+        document.getElementById("textb").innerText="STOP YAWNING , ITS JUST AFTERNOON ,GET SOME TEA  !!."
+        document.getElementById("texta").innerText="Good Evening !!"
         
     }
-    if(gh==hr){
-        // document.getElementById("image").src="n8time.jpg";
-        document.getElementById("box7").innerText='CLOSE YOUR EYES AND GO TO SLEEP !!.';
-        document.getElementById("box3").innerText="Good Night";
+    if(j==hour){
+        document.getElementById("image").style.backgroundImage="url(./gdn8.svg)"
+        document.getElementById("textb").innerText="CLOSE YOUR EYES AND GO TO SLEEP !!."
+        document.getElementById("texta").innerText="Good Night"
         
     }
+    getOption()
+}
+setAlarm()
 
+function getOption() {
+
+    var e = document.getElementById("wu");
+    var text1 = e.options[e.selectedIndex].text;
+    
+    var f = document.getElementById("lun");
+    var text2 = f.options[f.selectedIndex].text;
+    
+    var g = document.getElementById("np");
+    var text3 = g.options[g.selectedIndex].text;
+
+    var h = document.getElementById("n8");
+    var text4 = h.options[h.selectedIndex].text;
+
+    var idExist = document.getElementById("helo");
+    if(idExist){
+        idExist.innerHTML = `Wake Up Time : ${text1} <br> Lunch Time : ${text2}<br> Nap Time : ${text3} <br> Night Time:${text4}`;
+    }
 }
